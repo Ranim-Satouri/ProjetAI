212c16a4a242e93f5baf0a057462b56695f82b52
ranim <ranimsatouri23@gmail.com>
2024-05-29 18:55:03 +0100

    zedna des commentaires

--- BFS.py
+++ BFS.py
@@ -3,43 +3,59 @@ import heapq
 class BFSSolver:
     @staticmethod
     def solve_puzzle(initial_state, goal_state):
-        
+        # Initialisation de la file de priorité (frontier)
         frontier = []
         heapq.heappush(frontier, (0, initial_state))
+        # Dictionnaire pour stocker d'où vient chaque état
         came_from = {initial_state: None}
+        # Compteur pour le nombre d'itérations
         iterations = 0
 
+        # Boucle principale : continue tant que la file n'est pas vide
         while frontier:
+            # Retirer l'état avec la plus petite priorité de la file
             _, current_state = heapq.heappop(frontier)
-            iterations+=1
+            # Incrémenter le compteur d'itérations
+            iterations += 1
+
+            # Vérifier si l'état actuel est l'état cible
             if current_state == goal_state:
-              
+                # Reconstituer le chemin vers l'état initial
                 path = []
                 while current_state in came_from:
                     path.insert(0, current_state)
                     current_state = came_from[current_state]
-                return path,  iterations
+                return path, iterations
 
+            # Générer les états voisins de l'état actuel
             for next_state in BFSSolver.get_neighbors(current_state):
+                # Vérifier si le voisin n'a pas été visité
                 if next_state not in came_from:
+                    # Ajouter le voisin à la file avec sa priorité (heuristique)
                     heapq.heappush(frontier, (BFSSolver.heuristic(next_state, goal_state), next_state))
+                    # Mettre à jour la relation 'came_from' pour le voisin
                     came_from[next_state] = current_state
-            
 
-        return None , iterations
+        # Si la boucle se termine sans trouver de solution, retourner None pour le chemin
+        return None, iterations
 
     @staticmethod
     def get_neighbors(state):
+        # Méthode pour générer les états voisins d'un état donné
         neighbors = []
         empty_tile_i, empty_tile_j = None, None
+        # Trouver la position de la case vide dans l'état
         for i in range(3):
             for j in range(3):
                 if state[i][j] == 0:
                     empty_tile_i, empty_tile_j = i, j
 
+        # Générer les états voisins en déplaçant la case vide dans toutes les directions possibles
         for move in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             new_i, new_j = empty_tile_i + move[0], empty_tile_j + move[1]
+            # Vérifier si le déplacement est valide (reste dans la grille)
             if 0 <= new_i < 3 and 0 <= new_j < 3:
+                # Créer un nouvel état en effectuant le déplacement
                 new_state = [list(row) for row in state]
                 new_state[empty_tile_i][empty_tile_j], new_state[new_i][new_j] = new_state[new_i][new_j], new_state[empty_tile_i][empty_tile_j]
                 neighbors.append(tuple(map(tuple, new_state)))
@@ -48,10 +64,12 @@ class BFSSolver:
 
     @staticmethod
     def heuristic(state, goal_state):
+        # Méthode pour calculer l'heuristique d'un état par rapport à l'état cible
         distance = 0
         for i in range(3):
             for j in range(3):
                 if state[i][j] != goal_state[i][j] and state[i][j] != 0:
+                    # Calcul de la distance de Manhattan pour chaque tuile
                     x, y = divmod(goal_state[i][j], 3)
                     distance += abs(x - i) + abs(y - j)
         return distance
--- Main.py
+++ Main.py
@@ -2,14 +2,15 @@ import tkinter as tk
 import random
 from tkinter import messagebox
 from a_star import AStarSolver
-from hill_climbing import HillClimbingSolver
 from collections import deque
 from BFS import BFSSolver
+
 class EightPuzzle:
     def __init__(self, master):
+        # Initialisation de la fenêtre principale
         self.master = master
         self.master.title("8-Puzzle Game")
-        self.master.geometry("290x405")
+        self.master.geometry("290x370")
         self.tiles = [[None for _ in range(3)] for _ in range(3)]
         self.empty_tile = (2, 2)
         self.initialize_puzzle()
@@ -17,6 +18,7 @@ class EightPuzzle:
         self.master.config(bg="#FFB6C1")
 
     def initialize_puzzle(self):
+        # Initialisation du puzzle en mélangeant les nombres
         numbers = list(range(1, 9)) + [0]
         random.shuffle(numbers)
         idx = 0
@@ -29,6 +31,7 @@ class EightPuzzle:
                     self.empty_tile = (i, j)
         
     def create_buttons(self):
+        # Création des boutons pour afficher les tuiles du puzzle
         self.buttons = [[None for _ in range(3)] for _ in range(3)]
         for i in range(3):
             for j in range(3):
@@ -38,28 +41,27 @@ class EightPuzzle:
                     self.buttons[i][j] = btn
                 else:
                     self.buttons[i][j] = None
+        # Bouton pour réinitialiser le puzzle
         reset_button = tk.Button(self.master, text="Reset", width=9, height=1, font=('Arial', 10), command=self.reset)
         reset_button.grid(row=3, column=1, padx=5, pady=5)
-
+        # Boutons pour exécuter les algorithmes de résolution
         a_star_button = tk.Button(self.master, text="A*", width=9, height=1, font=('Arial', 10), command=self.run_a_star)
-        a_star_button.grid(row=4, column=0, padx=5, pady=5)
-
-        hill_climbing_button = tk.Button(self.master, text="Hill Climbing", font=('Arial', 10), width=9, height=1, command=self.run_hill_climbing)
-        hill_climbing_button.grid(row=4, column=1, padx=5, pady=5)
+        a_star_button.grid(row=3, column=0, padx=5, pady=5)
 
         bfs_button = tk.Button(self.master, text="BFS", width=9, height=1, font=('Arial', 10), command=self.run_bfs)
-        bfs_button.grid(row=4, column=2, padx=5, pady=5)
-
+        bfs_button.grid(row=3, column=2, padx=5, pady=5)
             
     def reset(self):
-        print("Let's Do it Again !!")
+        # Méthode pour réinitialiser le puzzle
         self.shuffle()
 
     def shuffle(self):
+        # Méthode pour mélanger les tuiles du puzzle
         self.initialize_puzzle()
         self.update_ui_with_state(self.tiles)
         
     def update_ui_with_state(self, state):
+        # Méthode pour mettre à jour l'interface utilisateur avec un nouvel état du puzzle
         for i in range(3):
             for j in range(3):
                 if state[i][j] == 0:
@@ -75,29 +77,20 @@ class EightPuzzle:
                         self.buttons[i][j] = btn
 
     def animate_solution(self, solution ,time):
+        # Méthode pour animer la solution trouvée
         if solution:
             def step(index):
                 if index < len(solution):
                     self.update_ui_with_state(solution[index])
-                    self.master.after(time, step, index + 1)  # Update every 1000 ms
+                    self.master.after(time, step, index + 1)  # Mise à jour toutes les 'time' millisecondes
             step(0)
         else:
             messagebox.showinfo("Result", "No solution found.")
 
     def run_a_star(self):
-        print("A Star")
+        # Méthode pour exécuter l'algorithme A*
         initial_state = tuple(map(tuple, self.tiles))
-        # initial_state = ((2, 7, 3), (8, 5, 1), (6, 4, 0))
-        # self.update_ui_with_state(initial_state)
-        print("Etat initial:")
-        for s in initial_state:
-            print(s)
-        print("-------------")
         goal_state = ((1, 2, 3), (4, 5, 6), (7, 8, 0))
-        print("Etat final:")
-        for s in goal_state:
-            print(s)
-        print("-------------")
         solution, iterations = AStarSolver.solve_puzzle(initial_state, goal_state)
         self.animate_solution(solution,800)
         if solution:
@@ -115,19 +108,9 @@ class EightPuzzle:
             print("No solution found.")
 
     def run_bfs(self):
-        print("BFS")
+        # Méthode pour exécuter l'algorithme BFS
         initial_state = tuple(map(tuple, self.tiles))
-        # initial_state = ((2, 7, 3), (8, 5, 1), (6, 4, 0))
-        # self.update_ui_with_state(initial_state)
-        print("Etat initial:")
-        for s in initial_state:
-            print(s)
-        print("-------------")
         goal_state = ((1, 2, 3), (4, 5, 6), (7, 8, 0))
-        print("Etat final:")
-        for s in goal_state:
-            print(s)
-        print("-------------")
         solution, iterations = BFSSolver.solve_puzzle(initial_state, goal_state)
         self.animate_solution(solution,600)
         if solution:
@@ -144,33 +127,8 @@ class EightPuzzle:
         else:
             print("No solution found.")
 
-    def run_hill_climbing(self):
-        print("Hill Climbing")
-        initial_state = tuple(map(tuple, self.tiles))
-        # initial_state = ((2, 7, 3), (8, 5, 1), (6, 4, 0))
-        # self.update_ui_with_state(initial_state)
-        print(initial_state)
-        goal_state = ((0, 1, 2), (3, 4, 5), (6, 7, 8))
-        print(goal_state)
-        
-        # Appel de la fonction solve_puzzle et décomposition du tuple retourné
-        solution, iterations = HillClimbingSolver.solve_puzzle(initial_state, goal_state)
-        
-        if solution:
-            print("Solution found:")
-            self.animate_solution(solution)
-            for i, step in enumerate(solution, start=1):
-                print(f"Step {i}:")
-                for row in step:
-                    print(row)
-            print(f"Resolved in {iterations} steps")
-        else:
-            print("No solution found.")
-
-
 if __name__ == "__main__":
+    # Création de la fenêtre principale et démarrage de l'application
     root = tk.Tk()
     game = EightPuzzle(root)
     root.mainloop()
-
-
